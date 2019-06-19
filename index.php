274c61401e968107d6e82eb3df38ee42fb752f26
SUPRIYO DAS <supriyo15998@gmail.com>
2019-06-19 23:24:06 +0530

    Login form and footer

--- index.php
+++ index.php
@@ -36,11 +36,40 @@
     		<span class="sr-only">Next</span>
   		</a>
 	</div>
-	<div class="container">
-		<div class="row">
-<!-- 			{{ login panel }} -->
-		</div>
+	<br><br>
+	<div class="container" style="width: 70%">
+		<center>
+  			<form action="#">
+    			<div class="row">
+      				<div class="col-25">
+        				<label style="margin-left: 50px;">Username</label>
+      				</div>
+      				<div class="col-75">
+        				<input type="text" name="uname" placeholder="Enter username">
+      				</div>
+    			</div>
+    			<div class="row">
+      				<div class="col-25">
+        				<label style="margin-left: 50px;">Password</label>
+      				</div>
+      				<div class="col-75">
+        				<input type="password" name="password" placeholder="Enter password">
+      				</div>
+    			</div>
+    			<div class="row">
+    				<div class="col-75">
+      					<input type="submit" value="Submit">
+    				</div>
+    			</div>
+  			</form>
+		</center>
 	</div>
+	<br>
+	<footer>
+  		<div class="footer-copyright text-center py-3" style="font-size: 18px">Designed and Developed By ©
+    		<a href="https://www.linkedin.com/in/supriyo-das-0176a116a/" target="_blank">Supriyo Das</a>
+  		</div>
+	</footer>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
