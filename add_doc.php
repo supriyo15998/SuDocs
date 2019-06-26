25c28ad21956e05cf5babaa5b2cbc83b84988bb5
SUPRIYO DAS <supriyo15998@gmail.com>
2019-06-26 22:47:38 +0530

    Form added

--- add_doc.php
+++ add_doc.php
@@ -18,17 +18,38 @@
 <html>
 <head>
 	<title>Dashboard | SuDocs</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/form_style.css">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
 	<h1>CR Dashboard</h1>
 	<a href="logout_cr.php" class="btn btn-outline-warning" style="float: right">Logout</a>
 	<h2>Welcome <?php echo $_SESSION['cr']['name']; ?></h2>
-	<div class="container-fluid">
-		<a href="add_doc.php" class="btn btn-primary">Add a Document</a>
+	<div class="container-fluid bg">
+		<div class="row">
+			<div class="col-md-4 col-sm-4 col-xs-12"></div>
+			<div class="col-md-4 col-sm-4 col-xs-12">
+				<form class="form-container">
+                    <h2>Add New Document</h2>
+					<div class="form-group">
+						<label>Subject</label>
+						<!-- Drop down list here -->
+					</div>
+					<div class="form-group">
+						<label>Document Title</label>
+						<input type="text" class="form-control" placeholder="Email">
+					</div>
+					<div class="form-group">
+						<label>Upload Document</label>
+                        <!-- File upload here -->
+					</div>
+					<button type="submit" class="btn btn-success btn-block">Submit</button>
+				</form>
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-12"></div>
+		</div>
 	</div>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
