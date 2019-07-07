0291b1db35148f743d224e188fbe2bcd6c67f71d
SUPRIYO DAS <supriyo15998@gmail.com>
2019-07-08 00:43:27 +0530

    document upload issue fixed / dynamically allocate the download links and configure max size

--- user_dashboard.php
+++ user_dashboard.php
@@ -26,11 +26,11 @@
 <body>
 	<h1>User Dashboard</h1>
 	<h2>Welcome <?php echo $_SESSION['user']['username']; ?></h2>
-	<a href="user_logout.php" class="btn btn-warning" style="float: right; margin-right: 40px;">Logout</a>
+	<a href="user_logout.php" class="btn btn-warning" style="float: right; margin-right: 40px; margin-top: 40px;">Logout</a>
 	<div class="container">
 		<div class="row">
-			<div class=col-md-5 style="margin-left: 10%">
-				<form action="" method="GET">
+			<div class=col-md-5 style="margin-left: 25%; margin-top: 40px;">
+				<form action="user_dashboard.php" method="GET">
 					<select name="subject">
 						<option autofocus value="">Choose Subject</option>
 						<?php
@@ -39,7 +39,7 @@
 							while($row=mysqli_fetch_array($exec))
 							{
 								?>
-								<option value="<?php echo $row['name']; ?>"><?php echo $row['name']; ?></option>
+								<option value="<?php echo $row['id']; ?>"><?php echo $row['name']; ?></option>
 								<?php
 							}
 						?>
@@ -47,18 +47,41 @@
 					<input type="submit" name="submit" class="btn btn-primary">
 				</form>
 			</div>
-		<div class="row">
-			<div class="col">
 			<?php
 				if(isset($_GET['submit']))
 				{
+					$id = $_GET['subject'];
+        			$qry1= "SELECT * FROM documents WHERE Subject_ID='$id'";
+					$exec1 = mysqli_query($conn,$qry1);
 					
-					$qry = "SELECT * FROM documents WHERE Subject_ID=''";
+					//var_dump($row1);
+					?>
+		</div>
+					<div class="row">
+					<?php
+						while($row1 = mysqli_fetch_array($exec1))
+						{
+					?>
+						
+							<div class="card" style="width: 18rem;">
+  								<img src="img/doc.jpg" class="card-img-top" alt="Download icon">
+  									<div class="card-body">
+    									<h5 class="card-title"><?php echo $row1['document_name']; ?></h5>
+										<!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+										<a href="#" class="btn btn-primary">Go somewhere</a>
+  									</div>
+							</div>
+						
+					<?php
+						}
+						?></div><?php
 				}
 			?>
-			</div>				
-		</div>
-		</div>
+		
+		<!-- card -->
+		
+		
+	<!-- card end -->
 	</div>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
