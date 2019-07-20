e2e1cbd99eba74b1d9b67b5446170ec844e6e999
supriyo15998 <supriyo15998@gmail.com>
2019-07-20 16:35:35 +0530

    minor fix

--- user_dashboard.php
+++ user_dashboard.php
@@ -61,6 +61,7 @@
 					<?php
 						while($row1 = mysqli_fetch_array($exec1))
 						{
+							//var_dump($row1['document_path']);
 					?>
 						
 							<div class="card" style="width: 18rem;">
@@ -68,7 +69,7 @@
   									<div class="card-body">
     									<h5 class="card-title"><?php echo $row1['document_name']; ?></h5>
 										<!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-										<a href="#" class="btn btn-primary">Go somewhere</a>
+										<a href="<?php echo $row1['document_path'] ?>" class="btn btn-primary">Download</a>
   									</div>
 							</div>
 						
